d996e4db34b638535bc166cd5b178c6d1ff0ef8f
codarsssss <codarsssss@gmail.com>
2024-02-06 15:45:07 +0300

    hot fix. забыл задать новую модель пользвателя для всег оприложения и добаить JWT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,11 +116,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+AUTH_USER_MODEL = 'users.User'
+
+
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'djoser.views.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }

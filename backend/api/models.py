2c3ef34e189520e1c1178710853c1c7f9d262acf
codarsssss <codarsssss@gmail.com>
2024-02-06 18:48:58 +0300

    добавил модель уведомления

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Notification(models.Model):
+    class Status(models.TextChoices):
+        INFO = 'INFO', 'Информационное'
+        WARNING = 'WARNING', 'Предупреждающее'
+        ERROR = 'ERROR', 'Сообщение об ошибке'
+
+    status = models.CharField(max_length=7, choices=Status.choices)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100) # надо подумать, нужен ли вообще title
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Уведомление'
+        verbose_name_plural = "Уведомления"
+
+    def __str__(self):
+        return f"{self.status}-{self.created_at}"

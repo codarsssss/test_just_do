6499c6cb74d98f7dcc0d0a685b61916133a7dc20
codarsssss <codarsssss@gmail.com>
2024-02-07 15:50:43 +0300

    поправил док

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,11 +8,10 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Cats API",
+      title="Notification API",
       default_version='v1',
-      description="Документация для приложения cats проекта Kittygram",
-      # terms_of_service="URL страницы с пользовательским соглашением",
-      contact=openapi.Contact(email="admin@kittygram.ru"),
+      description="Документация проекта Notification",
+      contact=openapi.Contact(email="codarsssss@yandex.ru"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,

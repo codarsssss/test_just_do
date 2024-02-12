eb7440a3b877cef2e539089876fd561a627b56e9
codarsssss <codarsssss@gmail.com>
2024-02-12 19:27:54 +0300

    front django работает, без статики

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- backend/users/views.py
+++ backend/users/views.py
@@ -17,12 +17,12 @@ class CustomUserViewSet(UserViewSet):
     # pagination_class = CustomPagination
 
 
-# class LogoutView(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def post(self, request, *args, **kwargs):
-#         r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
-#                         db=settings.REDIS_DB, decode_responses=True)
-#         user = request.user
-#         r.delete(f"user:{user.id}:token")
-#         return Response({"message": "Вы успешно вышли из системы."})
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
+                        db=settings.REDIS_DB, decode_responses=True)
+        user = request.user
+        r.delete(f"user:{user.id}:token")
+        return Response({"message": "Вы успешно вышли из системы."})
